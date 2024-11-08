9f74a15a1d8a096aaaf28c640b35b69670699783
50702247033 <50702247033@KLASSA302-29>
2024-11-08 10:59:58 +0200

    Pooleli

---
+++ KooliProjekt/Data/SeedData.cs
@@ -0,0 +1,25 @@
+﻿namespace KooliProjekt.Data
+{
+    public static class SeedData
+    {
+        public static void Generate(ApplicationDbContext context)
+        {
+            if (context.Client.Any())
+            {
+                Console.WriteLine("Kliendid on juba olemas");
+                return;
+            }
+
+            var Clients = new List<Client>
+            
+                    
+            
+
+                       
+
+            context.Client.AddRange(Clients);
+
+            context.SaveChanges();
+        }
+    }
+}
